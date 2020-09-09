767aa9b08a1e189a60dbf27cf89e0bf7dcbd84f7
Eshmanski <Eshmanski@gmail.com>
2020-09-09 17:15:33 +0300

    Добавлена мобильная верстка на основной странице + правки

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ $('.clients-slider').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
